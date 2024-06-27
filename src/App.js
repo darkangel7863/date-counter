b8103e69f5f30b8ac3239dad11cfa800ab3534a6
Mohamed El Sayed Ahmed Ismail <darkangel7863@gmail.com>
2024-06-27 04:13:50 +0300

    Rebuilding the component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import { useState } from 'react';
 
 export default function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+  return <Counter />;
 }
-
+// container
+// stepsCounter
+// countCounter
 function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
   const date = new Date();
   date.setDate(date.getDate() + count);
   return (
-    <div>
-      <div>
-        <button onClick={() => setStep(c => c - 1)}>-</button>
+    <div className="container">
+      <div className="stepsCounter">
+        <button onClick={() => setStep(s => s - 1)}>-</button>
         <span>Step:{step}</span>
-        <button onClick={() => setStep(c => c + 1)}>+ </button>
+        <button onClick={() => setStep(s => s + 1)}>+</button>
       </div>
-      <div>
+      <div className="countCounter">
         <button onClick={() => setCount(c => c - step)}>-</button>
         <span>Count:{count}</span>
-        <button onClick={() => setCount(c => c + step)}>+ </button>
+        <button onClick={() => setCount(c => c + step)}>+</button>
       </div>
       <p>
         <span>
